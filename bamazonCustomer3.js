be3715e3f5497110db3001e309eb2da5c90dfaa3
katherinejhwang <45325609+katherinejhwang@users.noreply.github.com>
2019-05-09 13:25:00 -0700

    Update bamazonCustomer3.js

--- bamazonCustomer3.js
+++ bamazonCustomer3.js
@@ -4,7 +4,7 @@ var mysql  = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'November2018!',
+  password : 'password',
   database : 'bamazonDB',
   port      : '3306'
 });
